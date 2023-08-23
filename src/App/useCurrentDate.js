388d1e95b4573b5887cb91319a7c2e9b9cccaef7
xtherexuu <therexuu@gmail.com>
2023-08-23 12:01:58 +0200

    created useCurrentDate hook

---
+++ src/App/useCurrentDate.js
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+const useCurrentDate = () => {
+    const [date, setDate] = useState(new Date);
+    useEffect(() => {
+        const intervalID = setInterval(() => {
+            setDate(new Date);
+        }, 1000)
+        return () => {
+            clearInterval(intervalID);
+        }
+    }, []);
+    return date;
+}
+
+export default useCurrentDate;
