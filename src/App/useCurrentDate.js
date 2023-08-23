640c945882765f72027d0d8ed68c3b7e1748236d
xtherexuu <therexuu@gmail.com>
2023-08-23 13:30:33 +0200

    clearing after merch

--- src/App/useCurrentDate.js
+++ src/App/useCurrentDate.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useEffect } from "react";
 import { useState } from "react";
 
@@ -16,23 +15,3 @@ const useCurrentDate = () => {
 }
 
 export default useCurrentDate;
-=======
-import React, { useEffect, useState } from "react";
-
-const useCurrentDate = () => {
-  const [date, setDate] = useState(new Date());
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setDate((date) => (date = new Date()));
-    }, 1000);
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, []);
-
-  return date;
-}
-
-export default useCurrentDate;
->>>>>>> b79760c8bd4c0e1beab1fd86d92d1fc76a512f77
