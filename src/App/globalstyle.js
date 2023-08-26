f657c3118d131caabdb9ef87f78bdef44d625927
Bartosz Załęski <bartosz.zaleski.pv@gmail.com>
2023-08-27 01:29:37 +0200

    added new font and keyframe

--- src/App/globalstyle.js
+++ src/App/globalstyle.js
@@ -1,5 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 import eina01font from "../Utils/Eina01.woff";
+import eina01fontbold from "../Utils/Eina01-Bold.woff";
 
 export const GlobalStyle = createGlobalStyle`
     @font-face {
@@ -8,6 +9,14 @@ export const GlobalStyle = createGlobalStyle`
       font-weight: normal;
       src: local("Eina01"),
         url(${eina01font}) format("woff");
+    }
+
+    @font-face {
+      font-family: "Eina01bold";
+      font-style: normal;
+      font-weight: normal;
+      src: local("Eina01"),
+        url(${eina01fontbold}) format("woff");
     }
 
     * {
@@ -22,31 +31,40 @@ export const GlobalStyle = createGlobalStyle`
     html {
       overflow-x: hidden;
       background-color: ${({ theme }) => theme.colors.mainBackgroundColor};
+      scroll-behavior: smooth;
     }
 
     :root {
-      @media (min-width: ${({theme}) => theme.breakpoints.smallmobile.breakpoint}) {
+      @media (min-width: ${({ theme }) =>
+        theme.breakpoints.smallmobile.breakpoint}) {
         font-size: 1em;
       }
-      @media (min-width: ${({theme}) => theme.breakpoints.mobile.breakpoint}) {
+      @media (min-width: ${({ theme }) =>
+        theme.breakpoints.mobile.breakpoint}) {
           font-size: 1.1em;
       }
-      @media (min-width: ${({theme}) => theme.breakpoints.tablet.breakpoint}) {
+      @media (min-width: ${({ theme }) =>
+        theme.breakpoints.tablet.breakpoint}) {
           font-size: 1.2em;
       }
-      @media (min-width: ${({theme}) => theme.breakpoints.mediumpc.breakpoint}) {
+      @media (min-width: ${({ theme }) =>
+        theme.breakpoints.mediumpc.breakpoint}) {
           font-size: 1.3em;
       }
-      @media (min-width: ${({theme}) => theme.breakpoints.largepc.breakpoint}) {
+      @media (min-width: ${({ theme }) =>
+        theme.breakpoints.largepc.breakpoint}) {
           font-size: 1.4em;
       }
-      @media (min-width: ${({theme}) => theme.breakpoints.fullhd.breakpoint}) {
+      @media (min-width: ${({ theme }) =>
+        theme.breakpoints.fullhd.breakpoint}) {
           font-size: 1.6em;
       }
-      @media (min-width: ${({theme}) => theme.breakpoints.quadhd.breakpoint}) {
+      @media (min-width: ${({ theme }) =>
+        theme.breakpoints.quadhd.breakpoint}) {
           font-size: 2.3em;
       }
-      @media (min-width: ${({theme}) => theme.breakpoints.ultrahd.breakpoint}) {
+      @media (min-width: ${({ theme }) =>
+        theme.breakpoints.ultrahd.breakpoint}) {
           font-size: 3em;
       }
     }
@@ -64,6 +82,17 @@ export const GlobalStyle = createGlobalStyle`
         /* transform: translate(50vh, -140vw); */
         bottom: 85%;
         right: 120%;
+      }
+    }
+
+    @keyframes scrollbuttonanimation {
+      0% {
+        opacity: 0.5;
+        transform: translateY(-10px);
+      }
+      100% {
+        opacity: 1;
+        transform: translateY(0);
       }
     }
 `;
